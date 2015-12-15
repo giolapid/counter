f1d4fd925d101bb15afb2309583f3283e0c516b4
Gio Lapid <giolapid@gmail.com>
2015-12-14 21:08:26 -0800

    added all the logic

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 
-function question(name, start){
-	var x = 3;	
+function question(name, start, end, inc){
+	var x = parseInt(inc);	
+	var st = parseInt(start);
+	var en = parseInt(end);
 	var text = "";
-	if(isNaN(name) == false){
-		alert("Please use letters for your name");
-		location.reload();
+
+	//Checks name if it is letters
+	if(!isNaN(name)){
+		alert("Please write your name");
 	}
-	if(start < 5 || start > 100){
-		alert("Please use a number between 5 to 100");
-		location.reload();
+
+	//checks if starting and ending numbers are between 5 and 100
+	if((st < 5 || st>100)||(en < 5 || en >100)){
+		alert("Please choose a number between 5 and 100");
 	}
-	else{ 
-		for (i = 0; i < start; i++) {
-			text += i + '<br>';
+
+	//checks if end<st
+	if(en < st){
+		alert("I CAN'T COUNT BACKWARDS");
+	}
+
+	//checks if we can increment it
+	if((en - st) < x){
+		alert("INCREMENT CAN'T BE GREATER THAN THE DIFFERENCE");
+	}
+	//logic for counting
+	else{
+		while(st < en){
+			text += st + "<br>";
+			st += x;
 		}
 	}
-	document.getElementById("intro").innerHTML= "Hello " + name;
-	document.getElementById("append").innerHTML= text;
+	document.getElementById('intro').innerHTML="Hello " + name + ",";
+	document.getElementById('append').innerHTML=text;
 };
